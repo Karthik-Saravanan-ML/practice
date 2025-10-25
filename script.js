5210e9ee975cb543cef1845b84eccb5e85cd1fc6
Karthik <karthik180708.com>
2025-10-25 13:48:48 +0530

    updated

---
+++ script.js
@@ -0,0 +1,8 @@
+var overlay=document.querySelector(".popup-overlay");
+var box=document.querySelector(".popup-box");
+var btn=document.getElementById(".add-button");
+btn.addEventListener("click",function(){
+    overlay.style.display="block";
+    box.style.display="block";
+});
+ 
